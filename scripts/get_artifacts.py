5f00a5645e4da74c160b0a9b47f0120609083938
minhquana <quann1906@gmail.com>
2025-11-12 19:37:29 +0700

    update final experiments and validation results

--- scripts/get_artifacts.py
+++ scripts/get_artifacts.py
@@ -1,8 +1,23 @@
 import wandb
 
-run = wandb.init()
-artifact = run.use_artifact(
-    "minhquana-university-of-transportation-and-communication/chest-xray-abnormality-detection/run_wxzus486_model:v0",
+run_light = wandb.init()
+artifact_light = run_light.use_artifact(
+    "minhquana-university-of-transportation-and-communication/chest-xray-abnormality-detection/run_ivmzqro0_model:v0",
     type="model",
 )
-artifact_dir = artifact.download()
+artifact_dir = artifact_light.download()
+
+
+run_hard = wandb.init()
+artifact_hard = run_hard.use_artifact(
+    "minhquana-university-of-transportation-and-communication/chest-xray-abnormality-detection/run_8rmuzba0_model:v0",
+    type="model",
+)
+artifact_dir = artifact_hard.download()
+
+run_baseline = wandb.init()
+artifact_baseline = run_baseline.use_artifact(
+    "minhquana-university-of-transportation-and-communication/chest-xray-abnormality-detection/run_bzu6cp8g_model:v0",
+    type="model",
+)
+artifact_dir = artifact_baseline.download()
